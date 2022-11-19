9bee0028735db0fd9c02457a7cb69f1e390532ef
aknjoroge <alexkaranja629@gmail.com>
2022-11-19 05:47:50 +0300

    initial setup

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeBaseProvider } from "native-base";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(function () {
+    async function loaded(params) {
+      await SplashScreen.hideAsync();
+    }
+    setTimeout(() => {
+      loaded();
+    }, 3000);
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,40 @@
+# A starter React Native App Template with EXPO
+
+
+### Initial Dependencies
+> async-storage
+> react-navigation - [ bottom-tabs, native, native-stack, drawer ] 
+> reduxjs - [ redux, toolkit, react-redux ]
+> expo-splash-screen
+> native-base
+
+
+### Core dependency versions
+
+- react: 18
+- react-native: 0.7
+- expo: 47
+ 
+
+## Usage
+
+```
+git clone https://github.com/aknjoroge/starter-react-native-setup.git
+cd starter-react-native-setup
+```
+
+```
+npm i --legacy-peer-deps
+```
+
+To update to the latest versions, edit the package.json file and remove ~ in the dependencies and replace with ^, <br/>
+then npm install again ie <br/>
+` "react": "~18.0.1",` <br/>
+To <br/>
+` "react": "^18.0.1",`
+
+### Script
+
+- npm start - start the development server
+- npm build - generate the app build files
+- npm test - run Jest tests
